0385820a0f59d912442619f548ef9b5e7f3536a9
Staark Inc <support@staarkinc.com>
2026-03-28 01:03:17 +0100

    fix: revert db.ts import to mysql2/promise

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,4 @@
-//import mysql from "mysql2/promise";
-import mysql from "/app/node_modules/mysql2/promise";
+import mysql from "mysql2/promise";
 
 export const db = mysql.createPool({
   host: process.env.DB_HOST,
