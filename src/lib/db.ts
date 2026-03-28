3592e2f886be5125a513088580c7faee454bf2f3
Staark Inc <support@staarkinc.com>
2026-03-28 01:16:13 +0100

    fix: use MYSQL_* env vars in db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,10 +1,10 @@
 import mysql from "mysql2/promise";
 
 export const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT || 3306),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQL_HOST,
+  port: Number(process.env.MYSQL_PORT || 3306),
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
   connectionLimit: 10,
 });
